f4cbffdb130ec80e18073581e1f9b9d1770b7ef0
suhara beevi <sairasuhara12345@gmail.com>
2023-11-08 14:09:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 
 import './App.css';
-import { useSelector,useDispatch } from 'react-redux';
-import {add,sub} from './redux/Actions'
+
+import DataFetching from './datafetching';
 
 function App() {
-  const  dispatch =useDispatch();
+ 
   return (
     <div className="App">
-      global value: {useSelector(state=>state)}
-      <br/>
-      <button onClick={()=>dispatch(add())}>+</button>
-      <br/>
-
-      <button  onClick={()=>dispatch(sub())}>-</button>
-
-
+      
+<DataFetching/>
     </div>
   );
 }
